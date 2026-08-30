b226528834833fc60cdf9db94660cc2e74d2c713
Koglesh R. Murugan <koglesh.official@gmail.com>
2026-08-30 15:46:57 +0800

    Ensure Google Search Console HTML verification tag is present

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,5 +19,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="en"><head><meta name="google-site-verification" content="LI6z3Avdq6RsVP2faZ6nlhcbRwvnMIdjJkrSBygvnZM" /></head><body>{children}</body></html>;
 }
